fc2976b4767bccb43ab1acc330c5e61fc8c09fb6
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-22 18:19:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-index.js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-fblsdt)
---
+++ index.js
@@ -0,0 +1,7 @@
+let a=3;
+let x=(100+50)* a;
+console.log(x);
+
+let p=4;
+console.log(p++);
+console.log(p);
