2d284083979565e93e4aadd0abee28e1d938500e
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-22 18:38:12 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ console.log(x);
 let p=4;
 console.log(p++);
 console.log(p);
+
+let b=3;
+console.log(b--);
+console.log(b);
+
+var c=5;
+c  +=5;
+console.log(c);
+
+var d=6;
+d*=2; 
+console.log(d);
+let e= 'mehak';
+let g=6;
+f=e+g;
+console.log(f);
+console.log(6==6); 
+console.log('5'==='5');
+console.log(5!=6);
+console.log("5"!=="6");
